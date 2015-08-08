de38ce76882762b199db81e5aac395de825b058b
Nipun Shrivastava <nipun.shrivastav@outlook.com>
2015-08-08 12:50:55 +0530

    8Aug15

---
+++ HR_EvenTree/HR_EvenTree/HR_EvenTree.cpp
@@ -0,0 +1,12 @@
+// HR_EvenTree.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+struct node
+{
+  int key_value;
+  node *left;
+  node *right;
+};
